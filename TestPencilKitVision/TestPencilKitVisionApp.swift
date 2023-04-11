68e64eb743b07c98858c7e2af953eca5863a7b17
Brayton-Lordianto <braylord30@gmail.com>
2023-04-10 20:16:58 -0400

    Initial Commit

---
+++ TestPencilKitVision/TestPencilKitVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPencilKitVisionApp.swift
+//  TestPencilKitVision
+//
+//  Created by Brayton Lordianto on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TestPencilKitVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
